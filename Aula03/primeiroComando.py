f6813469ebe4636a7d93e399fea2f346bb3806f0
BispoJPM <marcondesjpb@gmail.com>
2025-04-16 11:23:38 -0300

    16/04

---
+++ Aula03/primeiroComando.py
@@ -0,0 +1,16 @@
+# print('Ola, Mundo!')
+
+# a = 15
+# b = 7.5 
+
+# print(a + b, 'somando 15 com 7.5') # Soma
+
+# x = float(1.5)
+a = 'Isso é uma String com aspas Simples'
+
+b = "Isso é uma String com aspas Duplas"
+
+c = """Isso é Uma String com aspas Triplas"""
+
+print(a + "\n" + b + "\n" + c)
+
